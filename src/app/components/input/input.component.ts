7341e5557b86f0716d0b3e6661733c8d3a31d098
dcs member <sahil.arwand2000@gmail.com>
2024-05-13 14:48:15 +0500

    feat(app-input): create app-input component

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss',
+})
+export class InputComponent {
+  @Input() label: string = '';
+  @Input() type: string = '';
+  @Input() name: string = '';
+  @Input() error: boolean = false;
+  @Input() errorMessage: string = '';
+}
