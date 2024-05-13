c3c0abbd90c74ef476a1aff8c4e6b84a93f38fc3
dcs member <sahil.arwand2000@gmail.com>
2024-05-13 14:04:03 +0500

    feat(form): implemented validations on form

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -10,12 +15,48 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class FormComponent {
   contactForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    phone: new FormControl(null),
-    message: new FormControl(''),
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^\d+$/),
+    ]),
+    message: new FormControl('', Validators.required),
   });
+
+  constructor() {
+    this.logFormControlErrors();
+  }
+
+  logFormControlErrors() {
+    Object.keys(this.contactForm.controls).forEach((key) => {
+      const control = this.contactForm.get(key);
+      if (control) {
+        control.statusChanges.subscribe((status) => {
+          if (status === 'INVALID') {
+            const errors = control.errors;
+            console.error(`Validation errors for ${key}:`, errors);
+          }
+        });
+      }
+    });
+  }
+
+  markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach((control) => {
+      control.markAsTouched();
+
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      }
+    });
+  }
   onSubmit() {
-    console.log(this.contactForm.value);
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      this.contactForm.reset();
+    } else {
+      this.markFormGroupTouched(this.contactForm);
+    }
   }
 }
