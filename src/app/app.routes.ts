06eab84d294761d9fb644a611f87bd9756f79ff5
dcs member <sahil.arwand2000@gmail.com>
2024-05-13 12:40:28 +0500

    fix(style): fixed styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { SecondComponentComponent } from './components/second-component/second-component.component';
 
 export const routes: Routes = [
   { path: 'first', component: FirstComponentComponent },
+  { path: 'second', component: SecondComponentComponent },
 ];
